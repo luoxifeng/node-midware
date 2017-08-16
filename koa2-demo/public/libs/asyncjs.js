24e1701e6f7831aaccf4e44d8fae35b67875bcbb
luoxifeng <942394682@qq.com>
2017-08-16 18:06:03 +0800

    async js

---
+++ koa2-demo/public/libs/asyncjs.js
@@ -0,0 +1,36 @@
+;(function(win){
+
+    function Promise2(){
+        this.cbs = [];
+    }
+    Promise2.prototype.then = function(success){
+        this.cbs.push(success)
+    }
+
+    function asyncLoad (options) {
+        var res, script, opts;
+        if (!options) return console.error("AsyncJs require a string or object as argment");
+        if (typeof options === "string") {
+            opts = {"js": options};
+        } else if(Object.prototype.toString.call(options) === "[object Object]") {
+            opts = options
+            if (!opts.js) {
+                return console.error("opt must has a key which named 'js' as url");
+            }
+        } else {
+            return console.error("AsyncJs require a string or object as argment");
+        }
+
+        script = document.createElement("script");
+        script.src = opts.js;
+        script.onload = function(){
+            res.cbs[0]();
+        }
+        document.body.appendChild(script);
+        
+        return res = new Promise2();
+    }
+
+    $.AsyncJs = asyncLoad;
+
+})(window)
